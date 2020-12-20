3cfce81105034c90ed1c2a9d6a7d0a4411407bf8
Sakshyam Chapagain <chapagainsakshyam@gmail.com>
2020-12-20 14:08:33 +0545

    Added ability to add, subtract, divide and multiply

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let tempString = "";
 const screen = document.getElementById("screen");
 const buttons = document.getElementsByClassName("actionBtn");
 const backspace = document.getElementById("backspace");
-const equals = document.getElementById("equals");
+const equals = document.querySelector(".equals");
+const clear = document.getElementById("clear");
 
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", () => {
@@ -22,3 +23,50 @@ backspace.addEventListener("click", () => {
   screen.textContent = tempString;
   numbers.pop();
 });
+
+equals.addEventListener("click", () => {
+  result = parseInput(numbers.join(""));
+
+  screen.textContent = result;
+  numbers = [];
+  numbers.push(result);
+});
+
+function parseInput(str) {
+  if (str.includes("+")) {
+    output = add(str);
+    return output;
+  } else if (str.includes("-")) {
+    output = sub(str);
+    return output;
+  } else if (str.includes("*")) {
+    output = mult(str);
+    return output;
+  } else if (str.includes("/")) {
+    output = div(str);
+    return output;
+  }
+}
+
+function add(str) {
+  substr = str.split("+");
+  return Number(substr[0]) + Number(substr[1]);
+}
+
+function sub(str) {
+  substr = str.split("-");
+  return Number(substr[0]) - Number(substr[1]);
+}
+
+function mult(str) {
+  substr = str.split("*");
+  return Number(substr[0]) * Number(substr[1]);
+}
+
+function div(str) {
+  substr = str.split("/");
+  if (substr[1] == "0") {
+    return "Invalid";
+  }
+  return Number(substr[0]) / Number(substr[1]);
+}
